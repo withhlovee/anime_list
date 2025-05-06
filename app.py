04783daaee0a869123e1a48e91fc9c7910469999
Adityaa <167296292+withhlovee@users.noreply.github.com>
2025-05-06 19:25:05 +0530

    Update app.py
    added port

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))  # Default to port 4000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
